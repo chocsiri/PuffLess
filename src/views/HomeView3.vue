e1a7ab2028be9210fd081711e215d124f04f49ed
Danusorn6678 <150766076+Danusorn6678@users.noreply.github.com>
2025-02-13 19:37:52 +0700

    แก้ไขไฟล์เฉยๆ

---
+++ src/views/HomeView3.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Home View 3</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HomeView3",
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
+  
