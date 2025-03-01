8c52d85320205edfaa331d4772516619bcb5f11e
chocsiri <66022545@up.ac.th>
2025-03-02 00:12:19 +0700

    V 1.2

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,19 @@
 import { onMounted, ref } from 'vue';
 import { RouterView } from 'vue-router';
 
-
-import { useCartStore } from '@/stores/user/indexss.js';
-
-
 const cartData = ref([]);
 
-
-const cartStore = useCartStore();
-
-
 onMounted(async () => {
-  const data = await cartStore.loadCart();
+  // สมมติว่า fetchCartData() คือฟังก์ชันที่ดึงข้อมูลจาก API
+  const data = await fetchCartData();
   cartData.value = data;  
 });
+
 </script>
 
 <template>
   <div>
-    <h1>Cart Data</h1>
     <ul>
-  
       <li v-for="(item, index) in cartData" :key="index">
         {{ item }}
       </li>
@@ -30,4 +22,3 @@ onMounted(async () => {
     <RouterView />
   </div>
 </template>
-
